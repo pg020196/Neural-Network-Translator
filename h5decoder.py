7d33b9f1010d4792ade34295f4b22e8d8be28778
pg020196 <pg020196@gmail.com>
2019-08-10 18:34:50 +0200

    extended h5decoder with .json export functionality

--- h5decoder.py
+++ h5decoder.py
@@ -1,10 +1,11 @@
 import h5py
 import numpy as np
 import argparse
+import pandas as pd
 
 
 parser = argparse.ArgumentParser(description='Decodes .hdf5-files into value matrices.')
-parser.add_argument('-f', '--filepath', type=str, help='filepath of .hdf5-file')
+parser.add_argument('-i', '--input', type=str, help='filepath of input .hdf5-file')
 args = parser.parse_args()
 print(args)
 
@@ -14,6 +15,9 @@ class H5Decoder:
     def __init__(self,filepath):
         # Open .hdf5-file
         self.file = h5py.File(filepath,'r')
+        self.data1 = list()
+        self.data2 = list()
+
   
     def visitor_func(self, name, node):
         # Check whether the node is a dataset or a group
@@ -23,11 +27,16 @@ class H5Decoder:
             print(node)
             
             dataset = np.array(self.file.get(name))
-            
+            self.data1.append((name, dataset.tolist()))
+            self.data2.append((node, dataset.tolist()))
+
             print(dataset)
     
-    def read_hdf5_file(self):
+    def decode_hdf5_file(self):
         self.file.visititems(self.visitor_func)
+        pd.Series(self.data1).to_json(orient='values', path_or_buf='h5decoder_out_1.json')
+        pd.Series(self.data2).to_json(orient='values', path_or_buf='h5decoder_out_2.json')
+
     
-h5_decoder = H5Decoder(args.filepath)
-h5_decoder.read_hdf5_file()
+h5_decoder = H5Decoder(args.input)
+h5_decoder.decode_hdf5_file()
