49c3de29680a78ddf9300a2e43ac73cec0b5d09e
pg020196 <pg020196@gmail.com>
2019-09-27 12:54:11 +0200

    further refactoring #25

--- arduino-backend-output/arduino-backend-output.ino
+++ arduino-backend-output/arduino-backend-output.ino
@@ -4,33 +4,35 @@ void setup() {
 
   Serial.begin(9600); //opens serial port, sets data rate to 9600 bps
 
+  uint32_t ts1 = millis();
+
   int numberOfLayers = 3;
-  int unitsInLayer[3] = {8, 8, 1};
-  int maxUnitsInLayers = findmaxvalue();
-  int activationFunction = 3;
-  int weightsLayerLengths[] = {0, 64};
-  int biasLayerLenghts[] = {0, 8};
+  int unitsInLayers[3] = {8, 8, 1};
+  int activationFunctions[2] = {3, 3};
+  int startIndicesWeights[2] = {0, 64};
+  int startIndicesBias[2] = {0, 8};
+  bool useBias[2] = {true, true};
 
-  float input[unitsInLayer[0]] =
+  float input[unitsInLayers[0]] =
   {
-    1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0
+    6, 148, 72, 35, 0, 33.6, 0.627, 50
   };
 
-  float weights[(64+8)] =
+  float weights[(64 + 8)] =
   {
-      -0.15035194158554077, 1.430967926979065, 0.5391924381256104, -0.3918966054916382, 0.4462871849536896, 0.5552943348884583, 0.7700048089027405, 0.28981146216392517,
-      -1.689786672592163, 1.042858362197876, 2.2847230434417725, -1.9677045345306396, -0.06611120700836182, -0.061966150999069214, -0.6471585631370544, 1.4964995384216309,
-      0.19608011841773987, -0.6369050741195679, 0.09111300855875015, 0.13803385198116302, -0.3828596770763397, -0.4359494745731354, 0.7322076559066772, -0.049885910004377365,
-      0.4382251799106598, 0.7386993169784546, -0.7968388199806213, 0.40169671177864075, 1.0121262073516846, 0.2377474009990692, 0.4582376778125763, -0.26966771483421326,
-      0.1630777269601822, -0.869050920009613, -0.4613328278064728, 0.2624562084674835, -0.9985146522521973, -0.6456401944160461, -0.12137659639120102, 1.0982298851013184,
-      -0.03326864913105965, 0.6471387147903442, -0.1343340277671814, 0.22081176936626434, 0.854404628276825, -1.670728087425232, -0.16672520339488983, 0.8519517779350281,
-      1.2319378852844238, -0.01635606400668621, 0.28598904609680176, -0.34935298562049866, 0.049734167754650116, -1.2383219003677368, -0.7718221545219421, 0.7590272426605225,
-      0.3540281057357788, -1.3175017833709717, 1.9483529329299927, -2.1833057403564453, -1.0394269227981567, -0.08081339299678802, -2.6470179557800293, -0.5022009611129761,
-      0.4447292983531952, -0.8422787189483643, 0.8797550797462463, -0.8606524467468262, -0.1518070250749588, 0.2564888298511505, -0.13789983093738556, -0.3959729075431824
+    -0.15035194158554077, 1.430967926979065, 0.5391924381256104, -0.3918966054916382, 0.4462871849536896, 0.5552943348884583, 0.7700048089027405, 0.28981146216392517,
+    -1.689786672592163, 1.042858362197876, 2.2847230434417725, -1.9677045345306396, -0.06611120700836182, -0.061966150999069214, -0.6471585631370544, 1.4964995384216309,
+    0.19608011841773987, -0.6369050741195679, 0.09111300855875015, 0.13803385198116302, -0.3828596770763397, -0.4359494745731354, 0.7322076559066772, -0.049885910004377365,
+    0.4382251799106598, 0.7386993169784546, -0.7968388199806213, 0.40169671177864075, 1.0121262073516846, 0.2377474009990692, 0.4582376778125763, -0.26966771483421326,
+    0.1630777269601822, -0.869050920009613, -0.4613328278064728, 0.2624562084674835, -0.9985146522521973, -0.6456401944160461, -0.12137659639120102, 1.0982298851013184,
+    -0.03326864913105965, 0.6471387147903442, -0.1343340277671814, 0.22081176936626434, 0.854404628276825, -1.670728087425232, -0.16672520339488983, 0.8519517779350281,
+    1.2319378852844238, -0.01635606400668621, 0.28598904609680176, -0.34935298562049866, 0.049734167754650116, -1.2383219003677368, -0.7718221545219421, 0.7590272426605225,
+    0.3540281057357788, -1.3175017833709717, 1.9483529329299927, -2.1833057403564453, -1.0394269227981567, -0.08081339299678802, -2.6470179557800293, -0.5022009611129761,
+    0.4447292983531952, -0.8422787189483643, 0.8797550797462463, -0.8606524467468262, -0.1518070250749588, 0.2564888298511505, -0.13789983093738556, -0.3959729075431824
   };
 
 
-  float biases[8+1] =
+  float biases[8 + 1] =
   {
     0.4447292983531952, -0.8422787189483643, 0.8797550797462463, -0.8606524467468262, -0.1518070250749588, 0.2564888298511505, -0.13789983093738556, -0.3959729075431824,
     -0.2790255844593048
@@ -39,9 +41,8 @@ void setup() {
   // init with 1 since we don't work with the input layer
   for (int currentLayer = 1; currentLayer < numberOfLayers; currentLayer++)
   {
-
-    int numberOfPreviousUnits = unitsInLayer[currentLayer - 1];
-    int numberOfCurrentUnits = unitsInLayer[currentLayer];
+    int numberOfPreviousUnits = unitsInLayers[currentLayer - 1];
+    int numberOfCurrentUnits = unitsInLayers[currentLayer];
 
     //Number of rows in A
     int m = numberOfCurrentUnits;
@@ -55,102 +56,73 @@ void setup() {
         // printMatrix(1,p,weights[currentLayer-1][i]);
 
         // initialize with bias value
-        out[n * i + j] = biases[biasLayerLenghts[currentLayer-1]+i];
-        for (int k = 0; k < p; k++) {
-          out[n * i + j] = out[n * i + j] + weights[(p*i+k)+weightsLayerLengths[currentLayer-1]] * input[n * k + j];
-          Serial.print((p*i+k)+weightsLayerLengths[currentLayer-1]);
-          Serial.print(",");
+        out[n * i + j] = 0;
+        if (useBias[currentLayer - 1])
+        {
+          out[n * i + j] = biases[startIndicesBias[currentLayer - 1] + i];
         }
-        
-        out[n * i + j] = apply_activation_function(activationFunction, out[n * i + j]);
+
+        for (int k = 0; k < p; k++)
+        {
+          out[n * i + j] = out[n * i + j] + weights[(p * i + k) + startIndicesWeights[currentLayer - 1]] * input[n * k + j];
+        }
+        out[n * i + j] = apply_activation_function(activationFunctions[currentLayer - 1], out[n * i + j]);
       }
     }
-    Serial.print("====INPUT-BEFORE====");
-    printMatrix(1, numberOfPreviousUnits, input);
+    
     memset(input, 0, numberOfCurrentUnits);
-    //float input[numberOfCurrentUnits];
+    
     for (int i = 0; i < numberOfCurrentUnits; i++)
     {
       input[i] = out[i];
     }
-    Serial.print("====INPUT-AFTER====");
+
+    Serial.print("\n====OUTPUT LAYER "+String(currentLayer+1)+" ====");
     printMatrix(1, numberOfCurrentUnits, input);
-    //bias
-    //activation
+
   }
+  uint32_t ts2 = millis();
+  Serial.println("\ndone");
+  Serial.println(String(ts2 - ts1)+ " ms");
 }
+
+void loop() {}
 
 void printMatrix(int rows, int columns, float matrix[])
 {
-  Serial.println();
-
-  Serial.println();
-  for (int row = 0; row < rows; row++)
-  {
-    for (int column = 0; column < columns; column++)
+    Serial.println();
+  
+    Serial.println();
+    for (int row = 0; row < rows; row++)
     {
-      Serial.print(matrix[1 * row + column]);
-      Serial.print("   ");
+      for (int column = 0; column < columns; column++)
+      {
+        Serial.print(matrix[1 * row + column]);
+        Serial.print("   ");
+      };
+      Serial.print('\n');
     };
-    Serial.print('\n');
-  };
 }
 
-int findmaxvalue() {
-
-  return 8;
-  // go through array and return max value
-}
-void loop() {
-  /*
-    Number of units in layer as array: [4,1]
-    All biases in one matrix:
-    Biases   Layer 0  1
-                   x  x
-                   x  0
-                   x  0
-                   x  0
-    All weight_matrizes in weight_matrix_array
-
-    1. Check if input vector has same dimension (row or column) as weight matrix[0]
-    2. Create neuron_input_vector with length of array[0]
-    3. Copy input values to neuron_input_vector
-    4. Multiply weight_matrix_array[i=0] with neuron_input_vector
-      4a. Multiply entity
-      4b. Add bias
-      4b. Apply activationfunction
-    5. Store result in neuron_output_vector with length of [i+1=1]
-    6. Store neuron_output_vector in neuron_input_vector
-    7. Release neuron_output vector // Remove from memory
-    8. Go to 4.
-
-  */
-
-}
-
-void predict()
-{
-
-}
 
 float apply_activation_function(int actFunc, float input)
 {
   //RELU
   if (actFunc == 0)
   {
-    if (input > 0)
+    if (input <= 0)
     {
-      return input;
+      return 0;
     }
     else
     {
-      return 0;
+      return input;
     }
   }
   //SIGMOID
   else if (actFunc == 1)
   {
-    return 1 / (1 + exp(input));
+    return (1.0 / (1.0 + exp(-input)));
   }
   else
   {
