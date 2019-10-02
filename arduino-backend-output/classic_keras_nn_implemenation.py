b2140b0b374e39424aef0aa4ed438ba4db88f6a4
Christoph <christoph.brandl.de@gmail.com>
2019-10-02 15:17:59 +0200

    added classic keras implemenation for validation

---
+++ arduino-backend-output/classic_keras_nn_implemenation.py
@@ -0,0 +1,40 @@
+from keras.models import load_model
+import numpy as np
+from collections import OrderedDict
+from keras import backend as K
+from keras.models import Model
+from keras.models import Sequential
+from keras.layers import Dense
+
+# model = load_model('diabetes_model.h5')
+data = np.array([[6,148,72,35,0,33.6,0.627,50]])
+
+model = Sequential()
+model.add(Dense(8, input_dim=8, activation='sigmoid'))
+model.add(Dense(1, activation='sigmoid'))
+model.load_weights('diabetes_model.h5')
+
+weights_biases_dense_1 = model.layers[0].get_weights()
+weights_biases_dense_2 = model.layers[1].get_weights()
+
+model_dense_1 = Sequential()
+model_dense_1.add(Dense(8, input_dim=8, activation='sigmoid'))
+model_dense_1.set_weights(weights_biases_dense_1)
+print(weights_biases_dense_1)
+
+output_dense_1 = model_dense_1.predict(data)
+print(output_dense_1)
+
+model_dense_2 = Sequential()
+model_dense_2.add(Dense(1, input_dim=8, activation='sigmoid'))
+model_dense_2.set_weights(weights_biases_dense_2)
+print(weights_biases_dense_2)
+
+ouptut_dense_2 = model_dense_2.predict(output_dense_1)
+print(ouptut_dense_2)
+
+# output = model.predict(data)
+# print (output)
+
+
+
