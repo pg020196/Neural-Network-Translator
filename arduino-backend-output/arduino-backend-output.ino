d987bf7e1e5b3fa36a4fb84789641533b5fccb36
pg020196 <pg020196@gmail.com>
2019-10-05 15:22:00 +0200

    further refactoring #25 and implementation of #39

--- arduino-backend-output/arduino-backend-output.ino
+++ arduino-backend-output/arduino-backend-output.ino
@@ -5,7 +5,7 @@ extern "C" {
 void setup() {
 
   /*
-     Opens the serial port and sets the data rate to 9600 bps
+     Opens the serial port and sets the data rate to 9600 bps.
   */
   Serial.begin(9600);
   Serial.println("Enter input values:");
@@ -24,29 +24,32 @@ void loop()
     */
     inputString = Serial.readString();
 
+    /*
+          Print the received input string to allow verification.
+    */
     Serial.println("Input is: " + inputString);
 
     /*
-       Inits the input float array to zeros
-       The length of the array is determined through the number of units in layer 0 (input layer)
+       Inits the input float array with zeros.
+       The length of the array is determined through the number of units in layer 0 (input layer).
     */
     float input[unitsInLayers[0]] = {0};
     char *tmp;
     int i = 0;
     /*
-       Tokenize the string on "," charakter
+       Tokenize the string on "," characters.
     */
     tmp = strtok(&inputString[0], ",");
     while (tmp) {
       /*
-         convert a string token to a float
+         Convert a string token to a float and store it in the input array.
       */
       input[i++] = atof(tmp);
       tmp = strtok(NULL, ",");
     }
 
     /*
-      Call the predict function with the input array
+      Call the predict function with the input array.
     */
     predict(input);
 
@@ -57,7 +60,7 @@ void loop()
 void predict(float input[])
 {
   /*
-       Takes the starting time
+       Takes the starting time.
   */
   uint32_t startTime = micros();
 
@@ -66,6 +69,10 @@ void predict(float input[])
   */
   for (unsigned int currentLayer = 1; currentLayer < numberOfLayers; currentLayer++)
   {
+
+    /*
+      Init two variables to store the important values and improve readability.
+    */
     unsigned int numberOfPreviousUnits = unitsInLayers[currentLayer - 1];
     unsigned int numberOfCurrentUnits = unitsInLayers[currentLayer];
 
@@ -84,30 +91,60 @@ void predict(float input[])
     */
     float out[n] = {0};
 
+    /*
+       Loop through the number of units in the current layer.
+    */
     for (unsigned int i = 0; i < n; i++) {
+      /*
+         If bias is activated for the current layer, replace the initial value with the bias value.
+      */
       if (useBias[currentLayer - 1])
       {
         out[i] = biases[startIndicesBias[currentLayer - 1] + i];
       }
+      /*
+         Loop through the number of previous unit to perform the actual "matrix-multiplication".
+      */
       for (unsigned int j = 0; j < p; j++)
       {
         out[i] = out[i] + input[j] * weights[(n * j + i) + startIndicesWeights[currentLayer - 1]];
       }
-      out[i] = apply_activation_function(activationFunctions[currentLayer - 1], out[i]);
+      /*
+        Apply activation function directly to element to prevent having to loop through the array a second time.
+        If the activation function is equal to 0, no activation function shall be applied since 0 is defined as the linear activation function and thus does not change the value.
+        If the activation function is equal to 4, no activation function shall be applied since 4 is defined as the softmax activation function and thus must be applied after all values are calculated.
+      */
+      if (activationFunctions[currentLayer - 1] != 0 && activationFunctions[currentLayer - 1] != 4) {
+        out[i] = applyActivationFunction(activationFunctions[currentLayer - 1], out[i]);
+      }
     }
 
     /*
-       Re-init the input array with the dimenions of the current number of units
+       Apply the softmax activation function if necessary
+    */
+    if (activationFunctions[currentLayer - 1] == 4) {
+      applyActivationFunctionSoftmax(out, n);
+    }
+
+
+    /*
+       Re-init the input array with the dimenion of the current number of units
        and set initial value to 0
     */
     memset(input, 0, numberOfCurrentUnits);
 
+    /*
+      Fill the input array with the acutal calculated values
+    */
     for (unsigned int i = 0; i < numberOfCurrentUnits; i++)
     {
       input[i] = out[i];
     }
   }
 
+  /*
+     Stop the timer
+  */
   uint32_t endTime = micros();
   Serial.println(input[0], 8);
   Serial.println("done");
@@ -115,29 +152,50 @@ void predict(float input[])
 }
 
 /*
+   Applies the specified activation function for the given input value.
+   The activation functions can be called as follows:
    0: Linear
    1: Sigmoid
    2: ReLu
    3: Tanh
-   4: Softmax (not implemented)
+
+   To verify the implementation see:
+   https://en.wikipedia.org/wiki/Activation_function
 */
-float apply_activation_function(int actFunc, float input)
+float applyActivationFunction(int actFunc, float value)
 {
   switch (actFunc) {
     case 1:
-      return (1.0 / (1.0 + exp(-input)));
+      return (1.0 / (1.0 + exp(-value)));
     case 2:
-      if (input <= 0)
+      if (value <= 0)
         return 0;
       else
-        return input;
+        return value;
     case 3:
-      float expInput = exp(input);
-      float negExpInput = exp(-input);
+      float expInput = exp(value);
+      float negExpInput = exp(-value);
       return (expInput - negExpInput) / (expInput + negExpInput);
-    case 4:
-      return 0;
     default:
-      return input;
+      return value;
+  }
+}
+
+/*
+  IMPLEMENTATION NOT VERIFIED
+
+  Softmax requires a special implementation since the softmax function is applied on the complete vector.
+
+  To verify the implementation see:
+  https://en.wikipedia.org/wiki/Activation_function
+*/
+void applyActivationFunctionSoftmax(float values[], unsigned int inputLength) {
+  float denominator = 0;
+  for (int j = 0; j < inputLength; j++) {
+    denominator = denominator + exp(values[j]);
+  }
+  for (int i = 0; i < inputLength; i++) {
+    float divisor = exp(values[i]);
+    values[i] = divisor / denominator;
   }
 }
