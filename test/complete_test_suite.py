0016da6dd11abdbf11c145f9196b755b5d26355a
Christoph <christoph.brandl.de@gmail.com>
2019-11-07 20:01:00 +0100

    updated complete_test_suite #84

--- test/complete_test_suite.py
+++ test/complete_test_suite.py
@@ -2,18 +2,12 @@ import unittest
 import sys
 import argparse
 from test.backend_utils_test import TestBackendUtils
-from test.arduino_backend_test import TestArduinoBackend
+from test.gcc_backend_test import TestGCCBackend
 
 #? ############### INFO ###############
 #? This script has to be run with -m switch from parent directory in order to get the imports right
 #? Directory: Neural-Network-Translator (repository root directory)
 #? Command: python -m test.complete_test_suite
-
-parser = argparse.ArgumentParser(description='Neural Network Translator - TestRunner')
-parser.add_argument('-c', '--com', type=str, required=True, help='COM of Arduino')
-parser.add_argument('-b', '--baud', type=str, required=True, help='Baud rate of Arduino')
-parser.add_argument('-m', '--model', type=str, required=True, help='h5-model')
-args = parser.parse_args()
 
 test_loader = unittest.TestLoader()
 
@@ -29,16 +23,16 @@ for test_name in backend_utils_test_names:
 result_backend_utils = unittest.TextTestRunner().run(suite)
 print()
 
-#? Running tests for Arduino backend
-print('######################### Running tests for Arduino backend #########################')
+#? Running tests for GCC backend
+print('######################### Running tests for GCC backend #########################')
 
-#? Finding all test cases in TestArduinoBackend and executing the test suite
-arduino_backend_test_names = test_loader.getTestCaseNames(TestArduinoBackend)
+#? Finding all test cases in TestGCCBackend and executing the test suite
+gcc_backend_test_names = test_loader.getTestCaseNames(TestGCCBackend)
 suite = unittest.TestSuite()
-for test_name in arduino_backend_test_names:
-    suite.addTest(TestArduinoBackend(test_name, args.com, args.baud, args.model))
+for test_name in gcc_backend_test_names:
+    suite.addTest(TestGCCBackend(test_name))
 
-result_arduino_backend = unittest.TextTestRunner().run(suite)
+result_gcc_backend = unittest.TextTestRunner().run(suite)
 
 
-sys.exit(not (result_backend_utils.wasSuccessful() and result_arduino_backend.wasSuccessful()))
+sys.exit(not (result_backend_utils.wasSuccessful() and result_gcc_backend.wasSuccessful()))
