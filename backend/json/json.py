251c8be652b4d128838e12f31bb88ead335c491f
Christoph <christoph.brandl.de@gmail.com>
2019-11-09 17:26:30 +0100

    updated json backend so that output is also exported to _out folder

--- backend/json/json.py
+++ backend/json/json.py
@@ -1,5 +1,6 @@
 from plugin_collection import BackendPlugin
 import json
+import os
 
 class Json(BackendPlugin):
     """Json backend plugin returns the intermediate json format as string"""
@@ -7,7 +8,18 @@ class Json(BackendPlugin):
     def __init__(self):
         super().__init__('json', 'JSON Backend Plugin', ['float2int'])
 
-    def translate_to_native_code(self, input, outputfile):
+    def translate_to_native_code(self, input, outputfile, exec_file):
         """Returns the given json input as string representation"""
-        with open(outputfile, 'w') as file:
+        out_name, out_ext = os.path.splitext(outputfile)
+        out_dir = '_out/' + out_name
+
+        #? Creating directory if not existing
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
+
+        out = out_dir + '/' + outputfile
+        if (out_ext!='.json'):
+            out = out + '.json'
+
+        with open(out, 'w') as file:
             file.write(json.dumps(input))
