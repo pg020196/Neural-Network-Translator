bbba1e885c0b2352b940ba11a031a51efc472f44
pg020196 <pg020196@gmail.com>
2019-11-09 23:53:53 +0100

    refined padding code

--- conv/conv.ino
+++ conv/conv.ino
@@ -1,21 +1,43 @@
-extern "C"
-{
-#include "nn_model.h"
-}
-
 void setup() {
 
   /* Opens the serial port and sets the data rate to 9600 bps. */
   Serial.begin(9600);
+  test_padding();
+  Serial.println();
+  convolution_apply();
 
-
-  addpadding();
 }
 
-void conv()
+void test_padding()
 {
-  Serial.println("hi");
-  // TODO Implement convolution layer logic
+  //Input variables
+  uint16_t number_of_padding_layers = 0;
+  uint16_t input_width = 2;
+  uint16_t input_height = 2;
+  uint16_t output_width = input_width + 2 * number_of_padding_layers;
+  uint16_t output_height = input_height + 2 * number_of_padding_layers;
+
+  float * input = calloc(input_width * input_height, sizeof(float));
+  float data[input_width * input_height] = {1, 2, 3, 4};
+  for (uint16_t i = 0; i < input_width * input_height; i++)
+  {
+    *(input + i) = data[i];
+  }
+
+  float * result = padding_values_apply(input, input_width, input_height, number_of_padding_layers);
+
+  Serial.println();
+  for (uint16_t i = 0; i < output_height; i++) {
+    for (uint16_t j = 0; j < output_width; j++) {
+      uint16_t print_index = i * output_width + j;
+      Serial.print(String(*(result + print_index)) + "  ");
+    }
+    Serial.println();
+  }
+}
+
+void convolution_apply()
+{
   float input[64] = {7, 7, 6, 5, 5, 6, 7, 7, 7, 7, 6, 5, 5, 6, 7, 7, 6, 6, 4, 3, 3, 4, 6, 6, 5, 5, 3, 2, 2, 3, 5, 5, 5, 5, 3, 2, 2, 3, 5, 5, 6, 6, 4, 3, 3, 4, 6, 6, 7, 7, 6, 5, 5, 6, 7, 7, 7, 7, 6, 5, 5, 6, 7, 7};
   uint16_t input_width = 8;
   uint16_t input_height = 8;
@@ -35,7 +57,6 @@ void conv()
   uint16_t output_width = 6;
   float result;
   float * output = calloc(output_height * output_width, sizeof(float));
-  Serial.println("hi");
   //Loop through output matrix
   for (vertical_step_index = 0; vertical_step_index < output_height; vertical_step_index += vertical_stride)
   {
@@ -52,10 +73,8 @@ void conv()
           result = result + (*(input + input_index) * kernel[kernel_index]);
         }
       }
-      Serial.print(String(result) + ",");
       *(output + (horizontal_step_index + vertical_step_index * kernel_width)) = result;
     }
-    Serial.println();
   }
 
   for (int i = 0; i < output_height; i++) {
@@ -67,111 +86,102 @@ void conv()
   }
 }
 
-void addpadding()
+float * padding_values_apply(float * input, uint16_t input_width, uint16_t input_height, uint16_t number_of_padding_layers)
 {
-  //Input variables
-  int number_of_padding_layers = 1;
+  uint16_t current_padding_layer_index;
+  uint16_t output_width = input_width;
+  uint16_t output_height = input_height;
+  int16_t input_index;
+  int16_t output_index;
+  float * output;
+  uint16_t output_column_index;
+  uint16_t output_row_index;
 
-  //Local varibles
-  int currend_padding_layer;
-  for(current_padding_layer = 0; current_padding_layer < number_of_padding_layers; current_padding_layer++)
+  for (current_padding_layer_index = 0; current_padding_layer_index < number_of_padding_layers; current_padding_layer_index++)
   {
-    Serial.println();
-  int input_width = 6;
-  int input_height = 6;
-  int output_width = 8;
-  int output_height = 8;
+    input_index = 0;
+    output_width = output_width + 2;
+    output_height = output_height + 2;
+    output = calloc(output_width * output_height, sizeof(float));
 
-  float input[input_width * input_height] = {7,6,5,5,6,7,6,4,3,3,4,6,5,3,2,2,3,5,5,3,2,2,3,5,6,4,3,3,4,6,7,6,5,5,6,7};
-  float output[output_width * output_height];
-  
-  int counter = 0;
-  memset(output, 0, output_width * output_height * sizeof(float));
-
-  //Copy values from input to output
-  for (int o_h_index = 0; o_h_index < output_height; o_h_index++)
-  {
-    for (int o_w_index = 0; o_w_index < output_width; o_w_index++)
+    //Copy values from input to output
+    for (output_column_index = 0; output_column_index < output_height; output_column_index++)
     {
-      if(!(o_w_index < 1 || o_w_index > input_width || o_h_index < 1 || o_h_index > input_height))
+      for (output_row_index = 0; output_row_index < output_width; output_row_index++)
       {
-        int output_index = o_h_index*output_width + o_w_index;
-        output[output_index] = input[counter];
-        counter = counter + 1;
+        if (!(output_row_index < 1 || output_row_index > input_width || output_column_index < 1 || output_column_index > input_height))
+        {
+          output_index = output_column_index * output_width + output_row_index;
+          output[output_index] = input[input_index];
+          input_index = input_index + 1;
+        }
       }
     }
-  }
-  
-  // Copy upper row and extend corners
-  for(int i = 0; i < output_width; i++)
-  {
-    
-    int input_index = i-1;
-    if(input_index < 0){
-      input_index = 0;
-    }
-    if(input_index >= input_width)
+
+    // Copy upper row and extend corners
+    for (output_column_index = 0; output_column_index < output_width; output_column_index++)
     {
-      input_index = input_width - 1 ;
+      input_index = output_column_index - 1;
+
+      if (input_index < 0) {
+        input_index = 0;
+      }
+
+      if (input_index >= input_width)
+      {
+        input_index = input_width - 1 ;
+      }
+
+      output[output_column_index] = input[input_index];
     }
-    output[i] = input[input_index];
-  }
 
-  // Copy lower row and extend corners
-  for(int i = 0; i < output_width; i++)
-  {
-   int output_index = output_height*output_width-output_width+i;
-   Serial.print(String(output_index)+",");
-
-   int input_index = input_height*input_width-input_width+i-1;
-
-   if(input_index < input_height*input_width-input_width)
-   {
-    input_index = input_height*input_width-input_width;
-   }
-
-   if(input_index >= input_height*input_width){
-    input_index = input_height*input_width - 1;
-   }
-   
-   Serial.print(String(input_index)+",");
-   output[output_index] = input[input_index]; 
-
-   Serial.println();
-  }
-
-  //Extend left side
-  for(int i = 1; i < output_height - 1; i++)
-  {
-    int output_index = i*output_height;
-    int input_index = (i-1)*input_width;
-    if(input_index < 0)
+    // Copy lower row and extend corners
+    for (output_column_index = 0; output_column_index < output_width; output_column_index++)
     {
-      input_index = 0;
+      output_index = output_height * output_width - output_width + output_column_index;
+      input_index = input_height * input_width - input_width + output_column_index - 1;
+
+      if (input_index < input_height * input_width - input_width)
+      {
+        input_index = input_height * input_width - input_width;
+      }
+
+      if (input_index >= input_height * input_width) {
+        input_index = input_height * input_width - 1;
+      }
+
+      output[output_index] = input[input_index];
     }
-    output[output_index] = input[input_index]; 
-  }
 
-  // Extend right side
-  for(int i = 1; i < output_height - 1; i++)
-  {
-    int output_index = i*output_height+output_width-1;
-    int input_index = (i-1)*input_width+input_width-1;
-    
-    output[output_index] = input[input_index]; 
-  }
+    //Extend left side
+    for (output_row_index = 1; output_row_index < output_height - 1; output_row_index++)
+    {
+      output_index = output_row_index * output_height;
+      input_index = (output_row_index - 1) * input_width;
 
-  Serial.println();
-  for (int i = 0; i < output_height; i++) {
-    for (int j = 0; j < output_width; j++) {
-      int print_index = i * output_width + j;
-      Serial.print(String(output[print_index]) + "  ");
+      if (input_index < 0)
+      {
+        input_index = 0;
+      }
+
+      output[output_index] = input[input_index];
     }
-    Serial.println();
-  }
-  }
-  
 
+    // Extend right side
+    for (output_row_index = 1; output_row_index < output_height - 1; output_row_index++)
+    {
+      output_index = output_row_index * output_height + output_width - 1;
+      input_index = (output_row_index - 1) * input_width + input_width - 1;
+
+      output[output_index] = input[input_index];
+    }
+
+    input_width = output_width;
+    input_height = output_height;
+    free(input);
+    input = output;
+  }
+  return input;
 }
 
 void loop() {
